0b81057378e44a93a86ed3d863117052430cdf64
Bela Hausmann <75233+and3k@users.noreply.github.com>
2020-03-30 18:23:01 +0200

    updated tests for write_tsv

--- tests/testthat/test-write_tsv.R
+++ tests/testthat/test-write_tsv.R
@@ -1,10 +1,19 @@
 dt1 <- data.table::data.table(
-  String = c('foo', 'bar', 'Lorem ipsum dolor sit amet'),
-  Number = 1:3
+  String = c('foo', 'bar, baz', 'Lorem ipsum dolor sit amet', '', NA),
+  Number = c(1, NA, 2, 3, 4),
+  Bool = c(T, F, T, F, NA)
 )
 
-test_that('write_tsv() works', {
+test_that('write_tsv() works with data.table', {
   tf <- tempfile()
   write_tsv(dt1, tf)
-  expect_equal(dt1, data.table::fread(tf))
+
+  expect_equal(dt1, data.table::fread(tf, na.strings = ''))
+
+  df <- read.table(tf, header = TRUE, sep = '\t', quote = '', stringsAsFactors = FALSE)
+  expect_identical(df[1, 'String'], 'foo') # no quotations around normal strings
+  expect_identical(df[2, 'String'], 'bar, baz')
+  expect_identical(df[3, 'String'], 'Lorem ipsum dolor sit amet')
+  expect_identical(df[4, 'String'], '""') # empty strings are exported as empty quoted strings
+  expect_identical(df[5, 'String'], '') # NAs exported as empty
 })
