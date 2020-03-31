eaa9b7be7ebf501f3c091ddcbf415009894ba49e
Bela Hausmann <75233+and3k@users.noreply.github.com>
2020-03-31 18:17:42 +0200

    write_tsv man update

--- R/write_tsv.R
+++ R/write_tsv.R
@@ -1,16 +1,18 @@
 #' TSV writer
 #'
-#' As fwrite, but defaults to TSV instead of CSV.
+#' As [`fwrite`], but tab-separated (TSV) instead of the default comma-separated (CSV).
 #'
 #' @export
+#' @aliases fwrite_tsv
 #' @aliases write_tsv.data.table
-#' @param x An R object.
+#' @param x An \R object (e.g., a [`data.table`]).
 #' @param file Output file name. File extensions other than `.tsv`, `.tab`, and `.txt` result in a warning.
-#' @param ... Additional arguments to be passed to methods.
+#' @param ... Additional arguments to be passed to methods (e.g., to [`fwrite`]).
 #' @examples
 #' library(data.table)
-#' Mtcars <- as.data.table(mtcars, keep.rownames = TRUE)
-#' write_tsv(Mtcars, "mtcars.tsv")
+#' library(dtutils)
+#' mtcars_dt <- as.data.table(mtcars, keep.rownames = TRUE)
+#' write_tsv(mtcars_dt, "mtcars.tsv")
 write_tsv <- function(x, file, ...) {
   if(!all(stringr::str_detect(file, stringr::regex('\\.(tsv|tab|txt)$', ignore_case = TRUE)))) {
     warning('TSV files should have the extension \u2018.tsv\u2019.')
