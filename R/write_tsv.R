7c5d28ceaf9ee762f60d874016bcda94a7439038
Bela Hausmann <75233+and3k@users.noreply.github.com>
2020-03-31 17:45:01 +0200

    write_tsv now checks file extensions and separator

--- R/write_tsv.R
+++ R/write_tsv.R
@@ -5,17 +5,23 @@
 #' @export
 #' @aliases write_tsv.data.table
 #' @param x An R object.
-#' @param file Output file name.
+#' @param file Output file name. File extensions other than `.tsv`, `.tab`, and `.txt` result in a warning.
 #' @param ... Additional arguments to be passed to methods.
 #' @examples
 #' library(data.table)
 #' Mtcars <- as.data.table(mtcars, keep.rownames = TRUE)
 #' write_tsv(Mtcars, "mtcars.tsv")
 write_tsv <- function(x, file, ...) {
+  if(!all(stringr::str_detect(file, stringr::regex('\\.(tsv|tab|txt)$', ignore_case = TRUE)))) {
+    warning('TSV files should have the extension \u2018.tsv\u2019.')
+  }
   UseMethod('write_tsv')
 }
 
 #' @export
-write_tsv.data.table <- function(x, file, sep = "\t", ...) {
+write_tsv.data.table <- function(x, file, sep = '\t', ...) {
+  if(!identical(sep, '\t')) {
+    stop('The separator in TSV files needs to be \u2018\\t\u2019.')
+  }
   data.table::fwrite(x = x, file = file, sep = sep, ...)
 }
