801e11ddb580eab69704963af46c23a2a3ff7529
Bela Hausmann <75233+and3k@users.noreply.github.com>
2020-04-05 18:17:42 +0200

    first write_tsv() parameter changed from x to object

--- R/write_tsv.R
+++ R/write_tsv.R
@@ -1,11 +1,11 @@
 #' TSV writer
 #'
-#' Writes an \R object to a tab-separated (TSV) file.
+#' Writes an \R object to a tab-separated values (TSV) file.
 #'
-#' @param x An \R object to save as TSV.
+#' @param object An \R object to save as TSV.
 #' @param file Output file name.
 #'   File extensions other than `.tsv`, `.tab`, and `.txt` result in a warning.
-#' @param sep Defaults to `"\t"`. Cannot be changed.
+#' @param sep Defaults to `"\t"` and cannot be changed.
 #'   Included for comptability with base methods and underlying methods.
 #' @param ... Additional arguments to be passed to methods.
 #' @examples
@@ -14,7 +14,7 @@
 #' mtcars_dt <- as.data.table(mtcars, keep.rownames = TRUE)
 #' write_tsv(mtcars_dt, "mtcars.tsv")
 #' @export
-write_tsv <- function(x, file, sep = "\t", ...) {
+write_tsv <- function(object, file, sep = "\t", ...) {
   if (!all(stringr::str_detect(
     file,
     stringr::regex("\\.(tsv|tab|txt)$", ignore_case = TRUE)
@@ -27,9 +27,9 @@ write_tsv <- function(x, file, sep = "\t", ...) {
 #' @describeIn write_tsv Writes a [data.table] using [data.table::fwrite()].
 #' @aliases fwrite_tsv
 #' @export
-write_tsv.data.table <- function(x, file, sep = "\t", ...) { # nolint
+write_tsv.data.table <- function(object, file, sep = "\t", ...) { # nolint
   if (!identical(sep, "\t")) {
     stop("The separator in TSV files needs to be \u2018\\t\u2019.")
   }
-  data.table::fwrite(x = x, file = file, sep = sep, ...)
+  data.table::fwrite(x = object, file = file, sep = sep, ...)
 }
